945540af5ab852db3532ef595edb47932b6add48
ushakti07-bit <ushakti07@gmail.com>
2026-09-08 21:07:25 +0530

    Add files via upload

---
+++ day_13 q2.c
@@ -0,0 +1,11 @@
+//write a program to print number from 1 to n
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number:");
+scanf("%d",&n);
+
+for (int i=1; i<=n; i++){
+    printf("%d",i);}
+    return 0;
+}
