6b51580eeb41cc6454d8348312f8e4af3d37e4a6
ushakti07-bit <ushakti07@gmail.com>
2026-09-08 23:57:03 +0530

    Add files via upload

---
+++ day_15 q1.c
@@ -0,0 +1,13 @@
+//write a program to calculate the factorial of a number
+#include<stdio.h>
+int main(){
+    int n;
+    int fact =1;
+    printf("enter n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        fact =fact*i;
+    }
+    printf("factorial is :%d\n", fact);
+    return 0;
+}
