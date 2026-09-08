ca625ab8104d1c80119fa375cb63bc43f7e7f5d4
ushakti07-bit <ushakti07@gmail.com>
2026-09-09 02:31:35 +0530

    Add files via upload

---
+++ day_20 q1.c
@@ -0,0 +1,15 @@
+//write a program to find the product of odd digit of a number 
+#include<stdio.h>
+int main(){
+    int n,digit,product=1;
+    printf("enter a n");
+    scanf("%d",&n);
+    while(n>0){
+        (digit=n % 2!=0);
+        {product *= digit;}
+    n/=10; }
+    printf("no odd digits");
+    printf("(assume product of odd digits):%d\n",product);
+    return 0;
+    
+}
