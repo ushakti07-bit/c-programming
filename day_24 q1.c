b8b69cd5824c27bee1665db386be5d472d64e99d
ushakti07-bit <ushakti07@gmail.com>
2026-09-09 03:43:25 +0530

    Add files via upload

---
+++ day_24 q1.c
@@ -0,0 +1,17 @@
+/* write a program to print the following pattern*/
+*
+**
+***
+****
+*****
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=1:i<=5;i++){
+        for(j=1;j<=1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }return 0;
+}
+
---
+++ day_24 q2.c
@@ -0,0 +1,17 @@
+/*write a program to print the following pattern*/
+1
+12
+123
+1234
+12345*/
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;i++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }return 0;
+}
+        
