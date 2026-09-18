f7ae216735258cbc5998a3d518111bd91299d9c8
ushakti07-bit <ushakti07@gmail.com>
2026-09-18 20:24:46 +0530

    Add files via upload

---
+++ day_30 q2.c
@@ -0,0 +1,41 @@
+//count positive, negative, and zero elements in an array.
+#include<stdio.h>
+int main(void)
+{
+    int n;
+    int element;
+    int positive_count =0;
+    int negative_count =0;
+    int zero_count =0;
+    printf("enter the number of elements:");
+    scanf("%d",&n);
+    if (n<=0)
+    {
+        printf("array must contain at least one element.\n");
+        return 1;
+
+    }
+    printf(" enter %d elements:",n);
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&element);
+        if (element>0)
+        {
+            positive_count++;
+
+        }
+        else if (element<0)
+        {
+            negative_count++;
+        }
+        else{
+            zero_count++;
+
+        }
+        }
+        printf("positive number:%d\n", positive_count);
+        printf("negative numbers: %d\n",negative_count);
+        printf("zeros:%d\n",zero_count);
+        return 0;
+    }
+    
